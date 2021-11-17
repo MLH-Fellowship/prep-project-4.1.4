879d576ab44dc9d56bfd83c508b03fbefea1aa86
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2021-11-17 21:45:21 +0530

    autocomplete places

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import './App.css';
 import logo from './mlh-prep.png'
+import Autocomplete from "react-google-autocomplete";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
-
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
@@ -34,10 +34,17 @@ function App() {
       <img className="logo" src={logo} alt="MLH Prep Logo"></img>
       <div>
         <h2>Enter a city below 👇</h2>
-        <input
+        {/* <input
           type="text"
           value={city}
-          onChange={event => setCity(event.target.value)} />
+          onChange={event => onChangeCity(event)} /> */}
+        <Autocomplete
+          apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+          onPlaceSelected={(place) => {
+            setCity(place.formatted_address)
+          }}
+          defaultValue={city}
+        />
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
           {console.log(results)}

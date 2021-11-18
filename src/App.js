3ef156d8f22a86cc4347aadbd0c62fd99c2cc2a6
shravani05 <chavanshravani357@gmail.com>
2021-11-18 18:39:11 +0530

    Added geolocation

--- src/App.js
+++ src/App.js
@@ -5,10 +5,52 @@ import logo from './mlh-prep.png'
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
+  const [city, setCity] = useState("")
   const [results, setResults] = useState(null);
 
+  function getLocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+    else{
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function showPosition(position){
+    var lat=position.coords.latitude;
+    var lon=position.coords.longitude;
+  
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`)
+    .then(response => response.json())
+    .then(data => {
+      var currCity = data.city ? data.city : data.principalSubdivision
+      setCity(currCity)
+    })
+    .catch(error => alert(error))
+    
+  }
+  
+  function showError(error){
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+          alert("User denied the request for Geolocation.")
+          break;
+        case error.POSITION_UNAVAILABLE:
+          alert("Location information is unavailable.")
+          break;
+        case error.TIMEOUT:
+          alert("The request to get user location timed out.")
+          break;
+        case error.UNKNOWN_ERROR:
+          alert("An unknown error occurred.")
+          break;
+    }
+  }
+  
+
   useEffect(() => {
+    getLocation();
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
       .then(

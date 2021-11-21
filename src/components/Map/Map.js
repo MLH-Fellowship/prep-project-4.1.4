146f8ade6775fe12b50e44ed825c3c1082d5ac0c
Bhavika Gianey <bhavikagianey@gmail.com>
2021-11-21 19:05:43 +0530

    remove-extra-spaces

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,114 +1,61 @@
-import {
-  useEffect,
-  useState,
-  useRef
-} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup ,useMapEvent} from 'react-leaflet';
+import {MapContainer, TileLayer, Marker, Popup, useMapEvent} from 'react-leaflet';
 import "./Map.css";
 import 'leaflet/dist/leaflet.css';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
-});
+let DefaultIcon = L.icon({iconUrl: icon, shadowUrl: iconShadow});
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-
-const PlaceMarker = ({ city ,setCity, cityCoordinates, setCityCoordinates, map, setMap}) =>{
+const PlaceMarker = ({
+  city,
+  setCity,
+  cityCoordinates,
+  setCityCoordinates,
+  map,
+  setMap
+}) => {
 
   useMapEvent("click", (e) => {
     console.log(e.latlng);
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${e.latlng.lat}&lon=${e.latlng.lng}&appid=${process.env.REACT_APP_APIKEY}`
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setCityCoordinates({lat : e.latlng.lat,lon : e.latlng.lng});
-          setCity(result.name);
-        },
-        (error) => {
-          window.alert("Location not found")
-        }
-      );
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${e.latlng.lat}&lon=${e.latlng.lng}&appid=${process.env.REACT_APP_APIKEY}`).then((res) => res.json()).then((result) => {
+      setCityCoordinates({lat: e.latlng.lat, lon: e.latlng.lng});
+      setCity(result.name);
+    }, (error) => {
+      window.alert("Location not found")
+    });
   });
 
-  return(
-  <Marker position={[cityCoordinates.lat,cityCoordinates.lon]}>
-   <Popup>
-     {city}
-   </Popup>
- </Marker>
-);
+  return (<Marker position={[cityCoordinates.lat, cityCoordinates.lon]}>
+    <Popup>
+      {city}
+    </Popup>
+  </Marker>);
 }
 
-const WMap = ({ city ,setCity, cityCoordinates, setCityCoordinates}) => {
+const WMap = ({city, setCity, cityCoordinates, setCityCoordinates}) => {
   const [map, setMap] = useState(null);
-  const [position, setPosition] = useState({
-    Lat: "",
-    Long: "",
-    zoom: 9,
-    City: ""
-  });
+  const [position, setPosition] = useState({Lat: "", Long: "", zoom: 9, City: ""});
 
-  useEffect(()=>{
-    setPosition({
-      Lat: cityCoordinates.lat,
-      Long: cityCoordinates.lon,
-      zoom: 9,
-      City: city
-    })
-  },[cityCoordinates]);
+  useEffect(() => {
+    setPosition({Lat: cityCoordinates.lat, Long: cityCoordinates.lon, zoom: 9, City: city})
+  }, [cityCoordinates]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setMap(map)
-  },[map])
+  }, [map])
 
+  return (<> < div > <MapContainer className="map" whenCreated={setMap
+} center={[position.Lat, position.Long]} zoom={2
+}>
+    <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 
-  return (
-    <>
-    <div>
+    <PlaceMarker city={city} setCity={setCity} cityCoordinates={cityCoordinates} setCityCoordinates={setCityCoordinates} map={map} setMap={setMap}/>
 
-      < MapContainer className= "map"
-      whenCreated={setMap}
-      center={[position.Lat, position.Long]}
-      zoom={2}
-      >
-
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-       />
-
-
-     <PlaceMarker
-       city = {city} setCity = {setCity} cityCoordinates = {cityCoordinates} setCityCoordinates= {setCityCoordinates}
-       map = {map}
-       setMap = {setMap}
-       />
-
-      </ MapContainer  >
-    </div>
-    </>
-  );
+  </ MapContainer > < /div>
+</ >);
 }
 export default WMap;
-
-    // geocoder.reverse(
-    //   e.latlng,
-    //   map.options.crs.scale(map.getZoom()),
-    //   (results) => {
-    //     if(results.length>0){
-    //       console.log("Hello ", results[0].name);
-    //       setCity(results[0].name);
-    //       setCenter([e.latlng.lat,e.latlng.lng]);
-    //     }
-    //     else{
-    //       window.alert("Location not found");
-    //     }
-    //   }
-    // );

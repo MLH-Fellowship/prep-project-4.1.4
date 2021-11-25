40006da0b19299d612d23b5dad09bd2047a633b9
shravani05 <chavanshravani357@gmail.com>
2021-11-25 17:39:41 +0530

    Add footer

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,12 +1,14 @@
 import "./MainPage.css";
 import CurrentCityWeather from "../../components/CurrentCityWeather/CurrentCityWeather";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <CurrentCityWeather />
+      <Footer />
     </>
   );
 };

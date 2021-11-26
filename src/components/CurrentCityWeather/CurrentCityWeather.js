bb11eb2d1cb14104ba25a9ca68a1e677600cf407
Bhavika23 <63343906+Bhavika-Gianey@users.noreply.github.com>
2021-11-26 19:33:05 +0530

    feat: map (#20)
    * map added
    
    * map-with-current-location-added
    
    * useeffect on lat
    
    * map positions using geocoder
    
    * Update package.json
    
    * Update package.json
    
    * update package.json
    
    * map-location
    
    * remove-extra-spaces
    
    * Add setView effect for map (geographical center and zoom)
    
    * Use WMap instead of Map in CurrentCityWeather.js
    
    * Delete extra import statement in MainPage.js
    
    * value changed in autocomplete
    
    * changes
    
    * geolocation added
    
    * update changes
    
    * resolve conflicts
    
    * Update CurrentCityWeather.js
    
    * reset .gitignore
    
    * fix-marker-position
    
    * limit zoom level
    
    * Update CurrentCityWeather.js
    
    * Update Map.js
    
    * Automatic zoom in only when user is very zoomed out
    
    Co-authored-by: helenduz <duh2@carleton.edu>
    Co-authored-by: NoahCardoza <noahcardoza@gmail.com>

--- src/components/CurrentCityWeather/CurrentCityWeather.js
+++ src/components/CurrentCityWeather/CurrentCityWeather.js
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef  } from "react";
 import "./CurrentCityWeather.css";
 import Autocomplete from "react-google-autocomplete";
 import Background from "../../data/BackGroundAccordingToWeather";
 import { Col, Row } from "react-bootstrap";
+import WMap from "../../components/Map/Map";
 import HourlyCityWeather from "../HourlyCityWeather/HourlyCityWeather";
 import SongRecommendation from "../SongRecommendation/SongRecommendation";
 
 const CurrentCityWeather = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [cityCoordinates, setCityCoordinates] = useState({ lat: '40.7128', lon: '-74.0060' });
+  const [cityCoordinates, setCityCoordinates] = useState({lat: '40.7128', lon: '-74.0060'});
   const [results, setResults] = useState(null);
   const [city, setCity] = useState('New York');
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
+  function getLocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
     }
-    else {
+    else{
       alert("Geolocation is not supported by this browser.");
     }
   }
 
-  function showPosition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-
+  function showPosition(position){
+    const lat=position.coords.latitude;
+    const lon=position.coords.longitude;
     fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`)
-      .then(response => response.json())
-      .then(data => {
-        var currCity = data.city ? data.city : data.principalSubdivision
-        setCity(currCity)
-      })
-      .catch(error => alert(error))
-
-  }
+    .then(response => response.json())
+    .then(data => {
+      const currCity = data.city ? data.city : data.principalSubdivision
+      setCity(currCity)
+      setCityCoordinates({lat: lat,lon: lon});
+    })
+    .catch(error => alert(error))}
 
   function showError(error){
     switch(error.code){
@@ -47,7 +46,7 @@ const CurrentCityWeather = () => {
         case error.TIMEOUT:
           alert("The request to get user location timed out.")
           break;
-        default:
+        case error.UNKNOWN_ERROR:
           alert("An unknown error occurred.")
           break;
     }
@@ -55,21 +54,20 @@ const CurrentCityWeather = () => {
 
   useEffect(() => {
     getLocation()
-  })
+  }, [])
 
   useEffect(() => {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?lat=" +
-      cityCoordinates.lat +
-      "&lon=" +
-      cityCoordinates.lon +
-      "&units=metric" +
-      "&appid=" +
-      process.env.REACT_APP_APIKEY
+      "https://api.openweathermap.org/data/2.5/weather?"
+      + `lat=${cityCoordinates.lat}&`
+      + `lon=${cityCoordinates.lon}&`
+      + "units=metric&"
+      + `appid=${process.env.REACT_APP_APIKEY}`
     )
       .then((res) => res.json())
       .then(
         (result) => {
+          console.log(result);
           if (result["cod"] !== 200) {
             setIsLoaded(true);
             setError(result);
@@ -77,6 +75,7 @@ const CurrentCityWeather = () => {
             setIsLoaded(true);
             setError();
             setResults(result);
+            setCity(result.name + ", " + result.sys.country)
           }
         },
         (error) => {
@@ -89,14 +88,14 @@ const CurrentCityWeather = () => {
   }, [cityCoordinates]);
 
   function setCoordinates(place) {
-    var latitude = place.geometry.location.lat();
-    var longitude = place.geometry.location.lng();
-    var coordinates = { lat: latitude, lon: longitude };
+    const latitude = place.geometry.location.lat();
+    const longitude = place.geometry.location.lng();
+    const coordinates = {lat: latitude, lon: longitude};
     setCityCoordinates(coordinates);
   }
 
   function getCity(address) {
-    var addressComponents = address.split(",");
+    const addressComponents = address.split(",");
     setCity(addressComponents[0]);
   }
 
@@ -110,7 +109,7 @@ const CurrentCityWeather = () => {
             setCoordinates(place);
             getCity(place.formatted_address);
           }}
-          defaultValue={"New York, NY, USA"}
+          defaultValue={city}
           className="inputCity"
         />
         {error && (
@@ -125,65 +124,68 @@ const CurrentCityWeather = () => {
         )}
         {isLoaded && !error && results && (
           <>
-            <div
-              style={Background[results.weather[0].main]}
-              className="WeatherResults"
-            >
-              <div className="InnerWeatherResults">
-                <Row className="justify-content-center">
-                  <Col className="col-md-5 col-12">
-                    <div className="CurrentActualTemp">
-                      {results.main.temp}
-                      <sup>°C</sup>
+          <div
+            style={Background[results.weather[0].main]}
+            className="WeatherResults"
+          >
+            <div className="InnerWeatherResults">
+              <Row className="justify-content-center">
+                <Col className="col-md-5 col-12">
+                  <div className="CurrentActualTemp">
+                    {results.main.temp}
+                    <sup>°C</sup>
+                  </div>
+                  <div className="CurrentActualWeather">
+                    {results.weather[0].main}
+                  </div>
+                  <i>
+                    <div>
+                      {results.name}, {results.sys.country}
                     </div>
-                    <div className="CurrentActualWeather">
-                      {results.weather[0].main}
-                    </div>
-                    <i>
-                      <div>
-                        {city}, {results.sys.country}
+                  </i>
+                </Col>
+                <Col className="col-md-7 col-10">
+                  <Row className="flex-column justify-content-around">
+                    <Col className="pb-1 pt-2">
+                      <div className="currentTempDetails">
+                        <span> Feels like: </span> {results.main.feels_like}
+                        <sup>°C</sup>
                       </div>
-                    </i>
-                  </Col>
-                  <Col className="col-md-7 col-10">
-                    <Row className="flex-column justify-content-around">
-                      <Col className="pb-1 pt-2">
-                        <div className="currentTempDetails">
-                          <span> Feels like: </span> {results.main.feels_like}
-                          <sup>°C</sup>
-                        </div>
-                      </Col>
-                      <Col className="pb-2">
-                        <div className="currentTempDetails">
-                          <span> Humidity: </span>
-                          {results.main.humidity}%
-                        </div>
-                      </Col>
-                      <Col className="py-2">
-                        <Row>
-                          <Col>
-                            <div className="minmaxTempHeading">MIN</div>
-                            <div className="minmaxTemp">
-                              {results.main.temp_min} <sup>°C</sup>
-                            </div>
-                          </Col>
-                          <Col>
-                            <div className="minmaxTempHeading">MAX</div>
-                            <div className="minmaxTemp">
-                              {results.main.temp_max} <sup>°C</sup>
-                            </div>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
+                    </Col>
+                    <Col className="pb-2">
+                      <div className="currentTempDetails">
+                        <span> Humidity: </span>
+                        {results.main.humidity}%
+                      </div>
+                    </Col>
+                    <Col className="py-2">
+                      <Row>
+                        <Col>
+                          <div className="minmaxTempHeading">MIN</div>
+                          <div className="minmaxTemp">
+                            {results.main.temp_min} <sup>°C</sup>
+                          </div>
+                        </Col>
+                        <Col>
+                          <div className="minmaxTempHeading">MAX</div>
+                          <div className="minmaxTemp">
+                            {results.main.temp_max} <sup>°C</sup>
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
+               </Row>
               <Row>
                 <HourlyCityWeather city={city} />
               </Row>
               </div>
-            </div>
+            <WMap city = {city} setCity = {setCity} cityCoordinates = {cityCoordinates} setCityCoordinates= {setCityCoordinates}/>
+          </div>
+          <div>
             <SongRecommendation options={results} />
+          </div>
           </>
         )}
       </div>
